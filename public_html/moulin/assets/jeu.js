464c9b20ce2447395c78387b117263daea3ebae1
BuildTools <unconfigured@null.spigotmc.org>
2023-10-20 10:54:43 +0200

    animation de sélectionnement

--- public_html/moulin/assets/jeu.js
+++ public_html/moulin/assets/jeu.js
@@ -2,24 +2,6 @@ let tour = 1
 let id = null
 let plateau = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
 
-function joue(caseId) {
-    if (tour <= 18) {
-        console.log(tour)
-        console.log(caseId)
-        if (tour % 2 == 1) {
-            console.log(`pb${(tour+1)/2}`, `case${caseId}`)
-            mouvement(`pb${(tour+1)/2}`, `case${caseId}`)
-            console.log('blanc')
-            tour ++
-        } else {
-            console.log(`pn${tour/2}`, `case${caseId}`)
-            mouvement(`pn${tour/2}`, `case${caseId}`)
-            console.log('noir')
-            tour ++
-        }
-    }
-}
-
 function creer() {
     document.getElementById("optionsJouer").style.display = "none"
     document.getElementById("optionsCreer").style.display = "block" 
@@ -46,5 +28,32 @@ function mouvement(pion, place) {
     let xPlateau = posPlateau.x
     let yPlateau = posPlateau.y
     pionBouge.style.transform = `translate(${xBut + xPlateau - xPion}px, ${yBut + yPlateau - yPion}px)`
+}
+
+
+function joue(caseId) {
+    if (tour <= 18) {
+        console.log(tour)
+        console.log(caseId)
+        if (tour % 2 == 1) {
+            console.log(`pb${(tour+1)/2}`, `case${caseId}`)
+            mouvement(`pb${(tour+1)/2}`, `case${caseId}`)
+            console.log('blanc')
+            tour ++
+        } else {
+            console.log(`pn${tour/2}`, `case${caseId}`)
+            mouvement(`pn${tour/2}`, `case${caseId}`)
+            console.log('noir')
+            tour ++
+        }
+    }
+}
+
+function selectionne(pionId) {
+    if (tour > 18) {
+        document.getElementById(pionId).style['animation-name'] = "selectionner"
+        document.getElementById(pionId).style['animation-duration'] = "0.75s"
+        document.getElementById(pionId).style['animation-iteration-count'] = "2"
+    }
 
 }
