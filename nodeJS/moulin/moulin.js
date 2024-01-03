763c670b58f82e86c50879ece768ff8f0b6d6575
BuildTools <unconfigured@null.spigotmc.org>
2024-01-03 16:29:55 +0100

    création d'une id de partie

--- nodeJS/moulin/moulin.js
+++ nodeJS/moulin/moulin.js
@@ -6,30 +6,33 @@ module.exports = class Moulin {
       this.parties = {}
       this.duree
       this.couleur
+      this.messageDivise
     }
   
     connect(socket){
         
       socket.on("setup", (message) => {
-        messageDivise = message.split(":")
-        if (messageDivise[0] == "creationId") {
+        this.messageDivise = message.split(":")
+        if (this.messageDivise[0] == "creationId") {
+          console.log("création ID")
           this.id=Math.floor((Math.random()) * 1000000)
           while (this.id in this.parties) {
             this.id=Math.floor((Math.random()) * 1000000)
           }
+          socket.emit("info", "id:"+this.id)
+
         }
 
-        if (messageDivise[0] == "duree") {
-          this.duree = messageDivise[1]
+        if (this.messageDivise[0] == "duree") {
+          this.duree = this.messageDivise[1]
           socket.emit("test", this.duree)
         }
 
-        if (messageDivise[0] == "couleur") {
-          this.couleur = messageDivise[1]
+        if (this.messageDivise[0] == "couleur") {
+          this.couleur = this.messageDivise[1]
         }
 
         this.parties += new this.PartieMoulin(this.id, socket.id, this.duree, this.couleur)
-        socket.emit("info", "id:"+this.id)
       })
 
 }
--- public_html/moulin/assets/js/ligne.js
+++ public_html/moulin/assets/js/ligne.js
@@ -5,24 +5,27 @@ function init() {
     socket.on('info', (message) => {
         let separeMessage = message.split(":")
         if (separeMessage[0] == "id") {
-            console.log(separeMessage[1])
+            document.getElementById("optionsCreer").style.display = "none"
+            document.getElementById("votreId").innerText += separeMessage[1]
+            document.getElementById("creationId").style.display = "block"
+
         }
         if (separeMessage[0] == "commencer") {
-            utils.creerAppareil()    
+               
         }
-        })
-    socket.on("test", (message) => {
-        console.log(message)
     })
 
 }
 
 function creerLigne() {
     socket.emit("setup", "creationId:")
-    socket.emit("setup", `duree:${dureeJoueur}`)
-    socket.emit("setup", `couleur:${couleurJoueur}`)
+    socket.emit("setup", "duree:${dureeJoueur}")
+    socket.emit("setup", "couleur:${couleurJoueur}")
 }
 
+function rejoindre() {
+    socket.emit("connexion", "idConnexion:" + document.getElementById("noPartie").value)
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     init()
